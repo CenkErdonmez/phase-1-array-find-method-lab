c84053cb62c048c42027a795ea95e0522afad45d
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-04-18 15:29:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+// 
+
+
+ 
+  function superbowlWin(result){
+    const reResult = result.find(result=> result.result ==="W");
+    if(reResult){
+        return reResult.year
+    }else{
+        return
+    }
+
+}
